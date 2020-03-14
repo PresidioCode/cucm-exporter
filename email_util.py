82bd0d4311339f0112e1c7b81fb4b72671b6652d
Haas <bradh@presidio.com>
2020-03-13 22:09:09 -0400

    add email capabilities

---
+++ email_util.py
@@ -0,0 +1,83 @@
+import smtplib
+import mimetypes
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import os, pathlib
+
+
+template_folder = pathlib.Path().cwd() / 'TEMPLATE'
+
+def send_email(smtp_server="", send_to_email="", fileToSend='export.csv'):
+    """
+    Send Email Notification
+    """
+    from_email = "noreply@presidio.com"
+    subject = 'cucm-exporter job has completed'
+    
+    msg = MIMEMultipart('alternative')
+    msg['From'] = from_email
+    msg['To'] = send_to_email
+    msg['Subject'] = subject
+
+    with open(template_folder / "email-template.html", 'r') as myfile:
+        messageHTML = myfile.read().replace('\n', '')
+
+    messagePlain = f'''
+        cucm-exporter has completed a job...
+
+        Regards,
+        The cucm admin team
+        '''
+
+    ctype, encoding = mimetypes.guess_type(fileToSend)
+    if ctype is None or encoding is not None:
+        ctype = "application/octet-stream"
+
+    maintype, subtype = ctype.split("/", 1)
+
+    if maintype == "text":
+        fp = open(fileToSend)
+        # Note: we should handle calculating the charset
+        attachment = MIMEText(fp.read(), _subtype=subtype)
+        fp.close()
+    elif maintype == "image":
+        fp = open(fileToSend, "rb")
+        attachment = MIMEImage(fp.read(), _subtype=subtype)
+        fp.close()
+    elif maintype == "audio":
+        fp = open(fileToSend, "rb")
+        attachment = MIMEAudio(fp.read(), _subtype=subtype)
+        fp.close()
+    else:
+        fp = open(fileToSend, "rb")
+        attachment = MIMEBase(maintype, subtype)
+        attachment.set_payload(fp.read())
+        fp.close()
+        encoders.encode_base64(attachment)
+
+    attachment.add_header("Content-Disposition", "attachment", filename=fileToSend)
+    msg.attach(attachment)
+
+    msg.attach(MIMEText(messagePlain, 'plain'))
+    msg.attach(MIMEText(messageHTML, 'html'))
+
+    try:
+        server = smtplib.SMTP(smtp_server, 25)
+    except Exception as e:
+        return e.errno, e.strerror
+    text = msg.as_string()
+    try:
+        server.sendmail(from_email, send_to_email, text)
+        return('success')
+    except Exception as e:
+        return e.errno, e.strerror
+    server.quit()
+    
+
+if __name__ == "__main__":
+    email_send_status = send_email(smtp_server='mail.presidio.com', 
+                                    send_to_email='bradh@presidio.com', 
+                                    fileToSend='export.csv')
+    print(email_send_status)
