359918fa3aa770c1e5c3a2dbce048ddbb6cee819
Haas <bradh@presidio.com>
2020-03-16 22:29:02 -0400

    fix relative path for packaging.  This is way better than old path

--- email_util.py
+++ email_util.py
@@ -4,36 +4,36 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
-import pathlib
+from pathlib import Path
 from datetime import datetime
 
+BASE_DIR = Path(__file__).resolve().parent
+TEMPLATES_DIR = BASE_DIR.joinpath("TEMPLATE")
 
 
-template_folder = pathlib.Path().cwd() / 'TEMPLATE'
-
-def send_email(smtp_server="", send_to_email="", fileToSend='export.csv'):
+def send_email(smtp_server="", send_to_email="", fileToSend="export.csv"):
     """
     Send Email Notification
     """
     date_time = datetime.now().strftime("%m-%d-%Y_%H.%M.%S")
 
     from_email = "noreply@presidio.com"
-    subject = 'cucm-exporter job has completed'
-    
-    msg = MIMEMultipart('alternative')
-    msg['From'] = from_email
-    msg['To'] = send_to_email
-    msg['Subject'] = subject
+    subject = "cucm-exporter job has completed"
 
-    with open(template_folder / "email-template.html", 'r') as myfile:
-        messageHTML = myfile.read().replace('\n', '').replace(r'{date_time}', date_time)
+    msg = MIMEMultipart("alternative")
+    msg["From"] = from_email
+    msg["To"] = send_to_email
+    msg["Subject"] = subject
 
-    messagePlain = f'''
+    with open(TEMPLATES_DIR / "email-template.html", "r") as myfile:
+        messageHTML = myfile.read().replace("\n", "").replace(r"{date_time}", date_time)
+
+    messagePlain = f"""
         cucm-exporter has completed a job at {date_time}...
 
         Regards,
         The cucm admin team
-        '''
+        """
 
     ctype, encoding = mimetypes.guess_type(fileToSend)
     if ctype is None or encoding is not None:
@@ -64,8 +64,8 @@ def send_email(smtp_server="", send_to_email="", fileToSend='export.csv'):
     attachment.add_header("Content-Disposition", "attachment", filename=fileToSend)
     msg.attach(attachment)
 
-    msg.attach(MIMEText(messagePlain, 'plain'))
-    msg.attach(MIMEText(messageHTML, 'html'))
+    msg.attach(MIMEText(messagePlain, "plain"))
+    msg.attach(MIMEText(messageHTML, "html"))
 
     try:
         server = smtplib.SMTP(smtp_server, 25)
@@ -74,14 +74,16 @@ def send_email(smtp_server="", send_to_email="", fileToSend='export.csv'):
     text = msg.as_string()
     try:
         server.sendmail(from_email, send_to_email, text)
-        return('success')
+        return "success"
     except Exception as e:
         return e.errno, e.strerror
     server.quit()
-    
+
 
 if __name__ == "__main__":
-    email_send_status = send_email(smtp_server='mail.presidio.com', 
-                                    send_to_email='bradh@presidio.com', 
-                                    fileToSend='export.csv')
+    email_send_status = send_email(
+        smtp_server="mail.presidio.com",
+        send_to_email="bradh@presidio.com",
+        fileToSend="export.csv",
+    )
     print(email_send_status)
