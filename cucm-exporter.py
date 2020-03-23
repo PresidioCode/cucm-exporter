e75d54d427a768c88428ffb3f00cc55798dbdbfb
Haas <bradh@presidio.com>
2020-03-22 20:39:56 -0400

    add phone export capabilities

--- cucm-exporter.py
+++ cucm-exporter.py
@@ -187,11 +187,101 @@ def export_users(ucm):
     return all_users
 
 
-def export_phones():
+def export_phones(ucm):
     """
-    export phone details
+    Export Phones
     """
-    phone_list = ucm.get_phones(tagfilter={"userid": "",})
+    phone_list = ucm.get_phones(
+        tagfilter={
+            "name": "",
+            "description": "",
+            "product": "",
+            "model": "",
+            "class": "",
+            "protocol": "",
+            "protocolSide": "",
+            "callingSearchSpaceName": "",
+            "devicePoolName": "",
+            "commonDeviceConfigName": "",
+            "commonPhoneConfigName": "",
+            "networkLocation": "",
+            "locationName": "",
+            "mediaResourceListName": "",
+            "networkHoldMohAudioSourceId": "",
+            "userHoldMohAudioSourceId": "",
+            "loadInformation": "",
+            "securityProfileName": "",
+            "sipProfileName": "",
+            "cgpnTransformationCssName": "",
+            "useDevicePoolCgpnTransformCss": "",
+            "numberOfButtons": "",
+            "phoneTemplateName": "",
+            "primaryPhoneName": "",
+            "loginUserId": "",
+            "defaultProfileName": "",
+            "enableExtensionMobility": "",
+            "currentProfileName": "",
+            "loginTime": "",
+            "loginDuration": "",
+            # "currentConfig": "",
+            "ownerUserName": "",
+            "subscribeCallingSearchSpaceName": "",
+            "rerouteCallingSearchSpaceName": "",
+            "allowCtiControlFlag": "",
+            "alwaysUsePrimeLine": "",
+            "alwaysUsePrimeLineForVoiceMessage": "",
+        }
+    )
+
+    all_phones = []
+
+    for phone in phone_list:
+        # print(phone)
+        phone_details = {
+            "name": phone.name,
+            "description": phone.description,
+            "product": phone.product,
+            "model": phone.model,
+            "protocol": phone.protocol,
+            "protocolSide": phone.protocolSide,
+            "callingSearchSpaceName": phone.callingSearchSpaceName._value_1,
+            "devicePoolName": phone.defaultProfileName._value_1,
+            "commonDeviceConfigName": phone.commonDeviceConfigName._value_1,
+            "commonPhoneConfigName": phone.commonPhoneConfigName._value_1,
+            "networkLocation": phone.networkLocation,
+            "locationName": phone.locationName._value_1,
+            "mediaResourceListName": phone.mediaResourceListName._value_1,
+            "networkHoldMohAudioSourceId": phone.networkHoldMohAudioSourceId,
+            "userHoldMohAudioSourceId": phone.userHoldMohAudioSourceId,
+            "loadInformation": phone.loadInformation,
+            "securityProfileName": phone.securityProfileName._value_1,
+            "sipProfileName": phone.sipProfileName._value_1,
+            "cgpnTransformationCssName": phone.cgpnTransformationCssName._value_1,
+            "useDevicePoolCgpnTransformCss": phone.useDevicePoolCgpnTransformCss,
+            "numberOfButtons": phone.numberOfButtons,
+            "phoneTemplateName": phone.phoneTemplateName._value_1,
+            "primaryPhoneName": phone.primaryPhoneName._value_1,
+            "loginUserId": phone.loginUserId,
+            "defaultProfileName": phone.defaultProfileName._value_1,
+            "enableExtensionMobility": phone.enableExtensionMobility,
+            "currentProfileName": phone.currentProfileName._value_1,
+            "loginTime": phone.loginTime,
+            "loginDuration": phone.loginDuration,
+            # "currentConfig": phone.currentConfig,
+            "ownerUserName": phone.ownerUserName._value_1,
+            "subscribeCallingSearchSpaceName": phone.subscribeCallingSearchSpaceName._value_1,
+            "rerouteCallingSearchSpaceName": phone.rerouteCallingSearchSpaceName._value_1,
+            "allowCtiControlFlag": phone.allowCtiControlFlag,
+            "alwaysUsePrimeLine": phone.alwaysUsePrimeLine,
+            "alwaysUsePrimeLineForVoiceMessage": phone.alwaysUsePrimeLineForVoiceMessage,
+        }
+        all_phones.append(phone_details)
+
+        print(f"exporting: {phone.name}: {phone.model} - {phone.description}")
+
+    print("-" * 35)
+    print(f"number of phones: {len(all_phones)}")
+    return all_phones
 
 
 def main():
@@ -200,13 +290,13 @@ def main():
     if cli_args.cucm_export == "users":
         all_users = export_users(ucm)
         filename = output_filename(cli_args.filename)
-        write_csv(filename=filename, all_users=all_users)
-        print(f"status: elapsed time -- {datetime.now() - start_time}")
+        write_csv(filename=filename, data=all_users)
+        print(f"status: elapsed time -- {datetime.now() - start_time}\n")
     elif cli_args.cucm_export == "phones":
         all_phones = export_phones(ucm)
         filename = output_filename(cli_args.filename)
-        write_csv(filename=filename, all_users=all_phones)
-        print(f"status: elapsed time -- {datetime.now() - start_time}")
+        write_csv(filename=filename, data=all_phones)
+        print(f"status: elapsed time -- {datetime.now() - start_time}\n")
     else:
         print(f"exporting {cli_args.cucm_export} is not yet supported")
         return
